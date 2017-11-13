d391328eb13b1c7b6abd5ea8822d1b61a32321eb
ppking <ppking2897@gmail.com>
2017-11-13 18:43:37 +0800

    2017.11.13 測試UPDATE

--- app/src/main/java/com/example/shiningtechw/fusedlocationtest/MainActivity.java
+++ app/src/main/java/com/example/shiningtechw/fusedlocationtest/MainActivity.java
@@ -4,20 +4,24 @@ import android.Manifest;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.content.IntentSender;
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.location.LocationManager;
 import android.os.Looper;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.common.api.ResolvableApiException;
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationAvailability;
 import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationResult;
@@ -31,29 +35,68 @@ import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 
+import java.sql.Time;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
 public class MainActivity extends AppCompatActivity {
 
     private int REQUEST_PERMISSIONS_REQUEST_CODE = 123;
-    private FusedLocationProviderClient client ;
+    private FusedLocationProviderClient client;
     private SettingsClient settingsClient;
 
     private Location location;
-    private LocationRequest locationRequest;
+    private LocationRequest HIGH_ACCURACY_LocationRequest;
     private LocationSettingsRequest locationSettingsRequest;
     private LocationCallback locationCallback;
 
 
-    private long UPDATE_TIME_10_SEC = 10000;
+    private long UPDATE_TIME_SEC_FOR_HIGH_ACCURACY = 5000;
+    private long UPDATE_TIME_SEC_FOR_BALANCED_ACCURACY = 20000;
+    private long UPDATE_TIME_SEC_FOR_LOW_POWER = 30000;
+    private final int REQUEST_CHECK_SETTING = 1111;
+
+    private LocationRequest BALANCED_POWER_ACCURACY_LocationRequest;
+    private LocationRequest LOW_POWER_LocationRequest;
+
+    private LocationManager locationManager ;
+    private PackageManager packageManager ;
+
+
+    private TextView providerText , locationText , time;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        client = LocationServices.getFusedLocationProviderClient(this);
 
+        providerText = findViewById(R.id.provider);
+        locationText = findViewById(R.id.location);
+        time = findViewById(R.id.time);
+
+        locationManager = (LocationManager)this.getSystemService(Context.LOCATION_SERVICE);
+
+        packageManager = this.getPackageManager();
+
+
+
+        //Client 建構
+        client = LocationServices.getFusedLocationProviderClient(this);
+        //settingsClient 設定依時間取得座標
+        settingsClient = LocationServices.getSettingsClient(this);
         createLocationCallback();
-        createLocationRequest();
-        builderLocationSettingRequest();
+
+
+        createHIGHLocationRequest();
+//        createBALENCELocationRequest();
+//        createLOWPOWERLocationRequest();
+
+        builderAllLocationSettingRequest();
+        startLocationUpdates();
     }
 
     @Override
@@ -62,33 +105,48 @@ public class MainActivity extends AppCompatActivity {
         if (!checkPermissions()){
             requestPermissions();
         }else{
-            getLocation();
+            startLocationUpdates();
+            getLastLocation();
         }
 
     }
 
     @SuppressLint("MissingPermission")
-    public void getLocation(){
+    public void getLastLocation(){
         client.getLastLocation().addOnCompleteListener(this, new OnCompleteListener<Location>() {
             @Override
             public void onComplete(@NonNull Task<Location> task) {
                 if (task.isSuccessful()&&task.getResult()!=null){
                     location = task.getResult();
 
-//                    Log.v("ppking" , "location.getLatitude() : " + location.getLatitude());
-//                    Log.v("ppking" , "location.getLongitude() : " + location.getLongitude());
+                    Log.v("ppking" , "getLastLocation location.getLatitude() : " + location.getLatitude());
+                    Log.v("ppking" , "getLastLocation location.getLongitude() : " + location.getLongitude());
                 }else{
-                    Log.v("ppking" , "getLastLocation:exception  =", task.getException());
+                    Log.v("ppking" , "getLastLocation:exception  :"+ task.getException());
                 }
             }
         });
     }
 
-    private void createLocationRequest(){
-        locationRequest = new LocationRequest();
-        locationRequest.setInterval(UPDATE_TIME_10_SEC);
-        locationRequest.setFastestInterval(UPDATE_TIME_10_SEC/2);
-        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+    private void createHIGHLocationRequest(){
+        HIGH_ACCURACY_LocationRequest = new LocationRequest();
+        HIGH_ACCURACY_LocationRequest.setInterval(UPDATE_TIME_SEC_FOR_HIGH_ACCURACY);
+        HIGH_ACCURACY_LocationRequest.setFastestInterval(UPDATE_TIME_SEC_FOR_HIGH_ACCURACY /2);
+        HIGH_ACCURACY_LocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+    }
+
+    private void createBALENCELocationRequest(){
+        BALANCED_POWER_ACCURACY_LocationRequest = new LocationRequest();
+        BALANCED_POWER_ACCURACY_LocationRequest.setInterval(UPDATE_TIME_SEC_FOR_BALANCED_ACCURACY);
+        BALANCED_POWER_ACCURACY_LocationRequest.setFastestInterval(UPDATE_TIME_SEC_FOR_BALANCED_ACCURACY /2);
+        BALANCED_POWER_ACCURACY_LocationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+    }
+
+    private void createLOWPOWERLocationRequest(){
+        LOW_POWER_LocationRequest = new LocationRequest();
+        LOW_POWER_LocationRequest.setInterval(UPDATE_TIME_SEC_FOR_LOW_POWER);
+        LOW_POWER_LocationRequest.setFastestInterval(UPDATE_TIME_SEC_FOR_LOW_POWER /2);
+        LOW_POWER_LocationRequest.setPriority(LocationRequest.PRIORITY_LOW_POWER);
     }
 
     private void createLocationCallback(){
@@ -98,19 +156,72 @@ public class MainActivity extends AppCompatActivity {
                 super.onLocationResult(locationResult);
 
                 location = locationResult.getLastLocation();
-                Log.v("ppking" , "location.getLatitude() : " + location.getLatitude());
-                Log.v("ppking" , "location.getLongitude() : " + location.getLongitude());
 
+                Log.v("ppking" , "LocationCallBack location.getLatitude() : " + location.getLatitude());
+                Log.v("ppking" , "LocationCallBack location.getLongitude() : " + location.getLongitude());
+
+                checkProvider();
+
+                locationText.append(""+location.getLatitude() + "  " + location.getLongitude() +"\n");
+
+
+            }
+
+            @Override
+            public void onLocationAvailability(LocationAvailability locationAvailability) {
+                super.onLocationAvailability(locationAvailability);
+                Log.v("ppking" , "onLocationAvailability ! "  + locationAvailability.toString());
             }
         };
     }
 
-    private void builderLocationSettingRequest(){
+    private void startLocationUpdates(){
+        settingsClient.checkLocationSettings(locationSettingsRequest)
+                .addOnSuccessListener(this, new OnSuccessListener<LocationSettingsResponse>() {
+                    @SuppressLint("MissingPermission")
+                    @Override
+                    public void onSuccess(LocationSettingsResponse locationSettingsResponse) {
+                        checkProvider();
+                        client.requestLocationUpdates(HIGH_ACCURACY_LocationRequest, locationCallback, Looper.myLooper());
+                        Log.v("ppking" , "onSuccess !!");
+                    }
+                })
+                .addOnFailureListener(this, new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.v("ppking" , " onFailure !!" + e.toString());
+                        int statusCode = ((ApiException) e).getStatusCode();
+                        switch (statusCode) {
+                            case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+                                Log.i("ppking", "Location settings are not satisfied. Attempting to upgrade " +
+                                        "location settings ");
+                                try {
+                                    // Show the dialog by calling startResolutionForResult(), and check the
+                                    // result in onActivityResult().
+                                    ResolvableApiException rae = (ResolvableApiException) e;
+                                    rae.startResolutionForResult(MainActivity.this, REQUEST_CHECK_SETTING);
+                                } catch (IntentSender.SendIntentException sie) {
+                                    Log.i("ppking", "PendingIntent unable to execute request.");
+                                }
+                                break;
+                            case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
+                                String errorMessage = "Location settings are inadequate, and cannot be " +
+                                        "fixed here. Fix in Settings.";
+                                Log.e("ppking", errorMessage);
+                                Toast.makeText(MainActivity.this, errorMessage, Toast.LENGTH_LONG).show();
+                        }
+
+                    }
+                });
+    }
+
+    private void builderAllLocationSettingRequest(){
         LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder();
-        builder.addLocationRequest(locationRequest);
+        builder.addLocationRequest(BALANCED_POWER_ACCURACY_LocationRequest);
         locationSettingsRequest = builder.build();
     }
 
+    //權限要求
     private boolean checkPermissions() {
         int permissionState = ActivityCompat.checkSelfPermission(this,
                 Manifest.permission.ACCESS_COARSE_LOCATION);
@@ -123,43 +234,68 @@ public class MainActivity extends AppCompatActivity {
                 REQUEST_PERMISSIONS_REQUEST_CODE);
     }
 
-    private void startLocationUpdates(){
-        settingsClient.checkLocationSettings(locationSettingsRequest)
-                .addOnSuccessListener(this, new OnSuccessListener<LocationSettingsResponse>() {
-                    @SuppressLint("MissingPermission")
-                    @Override
-                    public void onSuccess(LocationSettingsResponse locationSettingsResponse) {
-                        client.requestLocationUpdates(locationRequest , locationCallback , Looper.myLooper());
-                    }
-                })
-                .addOnFailureListener(this, new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        int statusCode = ((ApiException) e).getStatusCode();
-                        switch (statusCode) {
-                            case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
-                                Log.v("ppking", "Location settings are not satisfied. Attempting to upgrade " +
-                                        "location settings ");
-
-                                break;
-                            case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
-                                String errorMessage = "Location settings are inadequate, and cannot be " +
-                                        "fixed here. Fix in Settings.";
-                                Log.v("ppking", errorMessage);
-                                Toast.makeText(MainActivity.this, errorMessage, Toast.LENGTH_LONG).show();
-                        }
-                    }
-                });
-    }
-
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         for (int grantResult : grantResults){
             if (grantResult != PackageManager.PERMISSION_GRANTED){
                 Toast.makeText(this , "請允許權限,否則無法定位" , Toast.LENGTH_SHORT).show();
             }else{
-                getLocation();
+                startLocationUpdates();
             }
         }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode){
+            case REQUEST_CHECK_SETTING :
+                switch (resultCode){
+                    case Activity.RESULT_OK:
+                        Log.v("ppking" , "RESULT_OK !!");
+                        startLocationUpdates();
+                        break;
+                    case Activity.RESULT_CANCELED:
+                        Log.v("ppking" , "RESULT_CANCELED !!");
+                        Toast.makeText(MainActivity.this , "請打開GPS定位!" , Toast.LENGTH_SHORT).show();
+                        startLocationUpdates();
+                        break;
+                }
+                break;
+
+        }
+    }
+
+    private void checkProvider(){
+        boolean gpsUsable = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        boolean gpsPresent = packageManager.hasSystemFeature(PackageManager.FEATURE_LOCATION_GPS);
+
+        boolean networkUsable = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+        boolean networkPresent =
+                packageManager.hasSystemFeature(PackageManager.FEATURE_LOCATION_NETWORK);
+
+//        boolean bleUsable = networkUsable;
+//        boolean blePresent = packageManager.hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE);
+
+
+        if (gpsPresent&&gpsUsable){
+            providerText.setText("GPS Provider\n");
+        }else if (networkPresent&&networkUsable){
+            providerText.setText("NET Provider\n");
+        }else {
+            providerText.append("no Provider\n");
+            Toast.makeText(this , "定位失敗，請檢查設備" , Toast.LENGTH_SHORT).show();
+        }
+
+
+
+        SimpleDateFormat formatter = new SimpleDateFormat("HH:mm:ss");
+
+        Date curDate = new Date(System.currentTimeMillis()) ; // 獲取當前時間
+
+        String str = formatter.format(curDate);
+
+        time.setText("經緯度 : \n "+str);
+//        Log.v("ppking"  , " checkProvider bleUsable : "+bleUsable);
+//        Log.v("ppking"  , " checkProvider blePresent : "+blePresent);
     }
 }
